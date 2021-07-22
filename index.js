96c9f0837a638ef2030d60ee1c46b3d1d83809e5
blake-mcclaer-auth0 <87400034+blake-mcclaer-auth0@users.noreply.github.com>
2021-07-22 10:19:59 +1000

    add auth middleware
    adds auth middleware to the index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const path = require("path");
 const { createServer } = require("http");
-// 👉 Replace this with express-openid-connect require 👈
+const { auth } = require("express-openid-connect"); //auth middleware from express-openid-connect node module.
 
 const {
   checkUrl,
